27179730141ee58c561f9ed95b5e5945bc73cec7
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-08-02 19:51:24 -0400

    Bring in Eleventy Base Blog with minor changes
    This adds almost all files from
    https://github.com/11ty/eleventy-base-blog
    
    Except for README and LICENSE.
    
    Also I edited some files and configuration to show my own information,
    and deleted extra posts.

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "FLC Pages",
+	url: "https://flc-pages.github.io/",
+	language: "en",
+	description: "Rants, opinions, technical posts, and maybe some tips.",
+	author: {
+		name: "Fernando L. Canizo",
+		email: "fernandocanizo@gmail.com",
+		url: "https://flc-pages.github.io/about-me/"
+	}
+}
---
+++ content/about/index.md
@@ -0,0 +1,11 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: About Me
+  order: 3
+---
+# About Me
+
+I'm Fernando L. Canizo, a parent, a programmer, a gardener, a builder, sometimes a handyman.
+
+I'm an Open Source advocate, eager to collaborate whenever an opportunity arises.
